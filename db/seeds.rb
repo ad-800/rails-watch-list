12faea53b537edebcbca8a5036effbadeaf70370
Adalyne Cummins <adalynecummins@gmail.com>
2022-08-18 17:06:04 +0100

    done with all the pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+puts "Planting seeds"
+
+country = 'pt'
+service = 'netflix' # prime, disney, hbo, hulu, peacock, paramount, starz, showtime, apple
+type = 'movie' # or series
+genre = ''
+page = 6
+url = URI("https://streaming-availability.p.rapidapi.com/search/basic?country=#{country}&service=#{service}&type=#{type}&genre=#{genre}&page=#{page}&output_language=en&language=en")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request['X-RapidAPI-Key'] = 'e4706810edmsh76a8aeb1b1ef015p170630jsneaab7251f736'
+request['X-RapidAPI-Host'] = 'streaming-availability.p.rapidapi.com'
+
+response = http.request(request)
+
+db = JSON.parse(response.read_body)
+
+db['results'].each do |result|
+  puts "Calculating #{result['title']}"
+
+  movie = { title: result['title'],
+            overview: result['overview'],
+            poster_url: "https://image.tmdb.org/t/p/original#{result['posterPath']}",
+            rating: result['imdbRating'] }
+
+  Movie.create(movie)
+end
+
+puts "Finished gardening"
